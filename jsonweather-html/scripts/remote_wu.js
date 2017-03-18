caf195e1ccdfac2aae11e46da0ebaaddaba8f107
mstong98 <mstong98@gmail.com>
2017-03-18 15:54:02 -0600

    current weather stuff @2

--- jsonweather-html/scripts/remote_wu.js
+++ jsonweather-html/scripts/remote_wu.js
@@ -23,7 +23,7 @@ $(function () {
     // Get the data from the wunderground API
     function getData(lat, long) {
         $.ajax({
-            url: "http://api.wunderground.com/api/5fe720d60e373978/geolookup/q/" + lat + "," + long + ".json",
+            url: "http://api.wunderground.com/api/5fe720d60e373978/geolookup/conditions/q/" + lat + "," + long + ".json",
             dataType: "jsonp",
             success: function (data) {
                 console.log(data);
